b7c4598d4d2aafa87ff838c70488abc183b728a4
Vincent Nguyen <vnguyen2@gmail.com>
2016-10-25 19:22:37 -0500

    added random praise and insult

--- 2server.js
+++ 2server.js
@@ -5,6 +5,12 @@ var http = require('http');
 var PORT=7000;
 var PORT2=7500;
 
+var feelGoodStatements = ["You are awesome!", "Great Job!", "thumbs up!", "Funny you are!"];
+var feelBadStatements = ["You are better off dead", "Trying is not for you", "You bore me", "You did great....NOT"]
+var randomIndex = Math.floor(Math.random() * feelGoodStatements.length);
+var randomFeelGood = feelGoodStatements[randomIndex];
+var randomFeelBad = feelBadStatements[randomIndex];
+
 //We need a function which handles requests and send response
 function handleRequest(request, response){
     response.end('It Works!! Path Hit: ' + request.url);
@@ -16,12 +22,11 @@ var server = http.createServer(handleRequest);
 //Lets start our server
 server.listen(PORT, function(){
     //Callback triggered when server is successfully listening. Hurray!
-    console.log("You are awesome only on port: %s", PORT);
+    console.log(randomFeelGood + " on port: %s", PORT);
 });
 
 server.listen(PORT2, function(){
     //Callback triggered when server is successfully listening. Hurray!
-    console.log("You suck on port: %s", PORT2);
+    console.log(randomFeelBad + " on port: %s", PORT2);
 });
 
-connnection.end();
